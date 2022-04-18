8bce877f46e2396cdb07779bd5252f7ae21a068f
kramstyles <mark.eke@decagon.dev>
2022-04-18 21:42:10 +0100

    added auth to settings

--- app/settings.py
+++ app/settings.py
@@ -37,6 +37,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
+    # Apps
+    'authentication.apps.AuthenticationConfig',
+
+    # PIP INSTALLS
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
