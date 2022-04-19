8b6a7dc8b3a5f539f65cf5a7086fb868739f848e
IFEANYI OMEATA <omeatai@gmail.com>
2022-04-19 20:59:23 +0100

    Created Model Classes
    Created Model Classes

--- authentication/admin.py
+++ authentication/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import (User, Currency, BaseQuoteSymbol, WalletType, Wallet, Rate)
 
-# Register your models here.
+
+admin.site.register(User)
+admin.site.register(Currency)
+admin.site.register(BaseQuoteSymbol)
+admin.site.register(WalletType)
+admin.site.register(Wallet)
+admin.site.register(Rate)
