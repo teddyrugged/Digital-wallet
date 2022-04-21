8dfab75270602355e70e5f6a4d65448f80480bab
kramstyles <mark.eke@decagon.dev>
2022-04-21 21:50:42 +0100

    added register serializer

---
+++ authentication/serializers.py
@@ -0,0 +1,16 @@
+from rest_framework import serializers
+
+from authentication.models import User
+
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(max_length=50, min_length=6, write_only=True)
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'password', 'token')
+        
+        read_only_fields = ['token']
+
+    def create(self, validated_data):
+        return User.objects.create(**validated_data)
