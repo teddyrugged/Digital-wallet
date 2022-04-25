fd0ed69c54bcab5e3302957abb56ae8574731c4e
kramstyles <mark.eke@decagon.dev>
2022-04-25 17:14:53 +0100

    created wallet

--- wallet/serializers.py
+++ wallet/serializers.py
@@ -4,6 +4,11 @@ from .models import Wallet
 
 
 class WalletSerializers(serializers.ModelSerializer):
+
     class Meta:
         model = Wallet
         exclude = ('username_id',)
+
+    def create(self, validated_data):
+        validated_data['username_id'] = self.context['username_id']
+        return Wallet.objects.create(**validated_data)
--- wallet/views.py
+++ wallet/views.py
@@ -1,4 +1,4 @@
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, response, status
 
 from . import serializers
 from .models import Wallet
@@ -8,3 +8,18 @@ class WalletApiView(generics.ListCreateAPIView):
     permission_classes = [permissions.IsAuthenticated]
     serializer_class = serializers.WalletSerializers
     queryset = Wallet.objects.all()
+
+    # def get_serializer_context(self):
+    #     context = super().get_serializer_context()
+
+    def get(self, request):
+        return response.Response(request.COOKIES)
+
+    def post(self, request):
+        serializer = self.serializer_class(data=request.data)
+        serializer.context['username_id'] = request.user
+        if serializer.is_valid():
+            serializer.save()
+            return response.Response({'message': 'Wallet Created', 'details': serializer.data}, status=status.HTTP_201_CREATED)
+        return response.Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
