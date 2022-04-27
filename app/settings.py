e847297bb8c6dd1afd7044438d562142cef51e32
Jagunmolu <kolawolejonathan@gmail.com>
2022-04-27 06:45:01 +0100

    Heroku config

--- app/settings.py
+++ app/settings.py
@@ -48,6 +48,7 @@ INSTALLED_APPS = [
     'login.apps.LoginConfig',
 
     # PIP INSTALLS
+    'whitenoise.runserver_nostatic',
     'rest_framework',
     'drf_yasg'
     
@@ -61,6 +62,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'app.urls'
@@ -129,7 +131,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
