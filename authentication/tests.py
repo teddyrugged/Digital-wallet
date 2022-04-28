4b0c27e9329e750204b222748e5d6bfa6d8bf3d8
IFEANYI OMEATA <omeatai@gmail.com>
2022-04-28 13:36:47 +0100

    Updated tests
    Updated tests

--- authentication/tests.py
+++ authentication/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from django.contrib.auth import get_user_model 
+from .models import Wallet
 
-# Create your tests here.
+
+class RegisterTests(TestCase):
+    
+    def setUp(self):
+            self.user = get_user_model().objects.create_user(
+            username='testuser',
+            email='test@email.com',
+            password='secret'
+        )
+        
+    def test_register_url_page(self):
+        response = self.client.get('/api/v1/register/')   
+        no_response = self.client.get('/posti/100000/')   
+        self.assertEqual(response.status_code, 200) 
+        self.assertEqual(no_response.status_code, 404) 
