d8b3ebfdef49aa88d87e27e3429df8544ce98d03
kramstyles <mark.eke@decagon.dev>
2022-04-28 23:25:07 +0100

    inherited restrictions from fund serializer

--- wallet/serializers.py
+++ wallet/serializers.py
@@ -31,21 +31,7 @@ class FundWalletSerializers(serializers.ModelSerializer):
         return super().validate(attrs)
 
 
-class WithdrawWalletSerializers(serializers.ModelSerializer):
+class WithdrawWalletSerializers(FundWalletSerializers):
     class Meta:
         model = Wallet
         fields = ['id', 'amount', 'currency_id']
-
-    def validate(self, attrs):
-        print(attrs)
-        amount = attrs.get('amount', '')
-        user = self.context['details'][0]
-        wallet_owner = Wallet.objects.get(pk=self.context['details'][1]).username_id
-
-        if not amount:
-            raise serializers.ValidationError('Amount should not be empty')
-        if user != wallet_owner:
-            raise serializers.ValidationError('You are not the owner of this Wallet! You cannot fund this wallet')
-        return super().validate(attrs)
-
-
