bab5a38f2dbba42f607f34b470f70eb30ac3e430
teddyrugged <jakpaetuk@gmail.com>
2022-08-11 12:19:34 +0100

    secretkey

--- app/settings.py
+++ app/settings.py
@@ -16,6 +16,7 @@ from pathlib import Path
 from dotenv import load_dotenv
 
 load_dotenv()
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -24,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*enyf2rqwgold+lc-x7^+*v3f(!oqi91-^xqx*qdu40srn5#3&'
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config("DEBUG",cast = bool)
 
 ALLOWED_HOSTS = ['*']
 
